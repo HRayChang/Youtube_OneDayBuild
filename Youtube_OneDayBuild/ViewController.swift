6eef48fccaed096a13b1663f5f0d93076f0590ee
Ray Chang <raychang@Rays-MacBook-Air.local>
2023-08-15 17:58:56 +0800

    initial xcode project creation

---
+++ Youtube_OneDayBuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube_OneDayBuild
+//
+//  Created by Ray Chang on 2023/8/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
